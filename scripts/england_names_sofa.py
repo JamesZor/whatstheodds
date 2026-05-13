d994760d7a88198b5421ab34238d303d77d8ccc0
James LP1 <jameszoryk@msn.com>
2026-05-13 11:44:23 +0100

    files clean up

--- scripts/england_names_sofa.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-file_path = (
-    "/home/james/bet_project/football/england_20_25/football_data_mixed_matches.csv"
-)
-
-df = pd.read_csv(file_path)
-
-print(sorted(df["home_team"].unique()))
